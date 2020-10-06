87c74680addb7786c789ba11f067017529d74538
celer7841 <celer7841@wrdsb.ca>
2020-10-06 12:45:06 +0000

    fınıshed

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * For loop example
+ * @author celer7841
  */
 public class Main {
 
@@ -9,7 +9,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create the counting variable
+    int count = 1;
+    // create our loop to count 
+    while(count <=5){
+      // print the number
+      System.out.println(count);
+      // add 1 to count
+      count = count +1;
+    }
+
+
+   // create a for loop that counts to 5
+   for(int count2 = 1; count2 <=5; count2 = count2 +1){
+     // print the number
+     System.out.println("Your number is " + (count2 + 1));
+   }
+
   }
 }
